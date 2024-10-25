b0bae7280fe192de1b14dc73b343fa3b58210318
Ranvi718 <shkjh0990@gmail.com>
2024-10-26 02:21:34 +0900

    gps Permission 추가

--- app/src/main/java/com/cookandroid/aerobicapplication/TitleActivity.java
+++ app/src/main/java/com/cookandroid/aerobicapplication/TitleActivity.java
@@ -1,19 +1,28 @@
 package com.cookandroid.aerobicapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class TitleActivity extends AppCompatActivity {
-
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.title_screen);
+
+        // gps 권한 부여
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
 
         Button log_in = (Button) findViewById(R.id.login);
         log_in.setOnClickListener(new View.OnClickListener() {
