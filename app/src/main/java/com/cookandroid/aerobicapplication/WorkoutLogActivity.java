3a3a4e02104d15ec1a67eb897a21fcc536739145
df2854 <df2854@icloud.com>
2024-11-05 15:02:44 +0900

    style: 경로 생성 -> 운동 기록으로 수정

---
+++ app/src/main/java/com/cookandroid/aerobicapplication/WorkoutLogActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.aerobicapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkoutLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_log);
+    }
+}
